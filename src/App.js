46bbcae3440507521087b6bce8453edb740c37ac
Satyanarayan Sen <60086933+Satyanarayan4434@users.noreply.github.com>
2023-06-27 12:11:09 +0530

    API data fetched

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { Table } from "./components/Table";
 function App() {
   return (
     <div className="flex flex-col w-[880px] mx-auto mt-10 gap-10">
-      <h1 className="mx-auto font-mono text-2xl">India's Covid-19 Data Satewise</h1>
+      <h1 className="mx-auto font-mono text-2xl">India's Covid-19 Data Statewise</h1>
       <Table/>
     </div>
   );
